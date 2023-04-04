556f856842b977d5c0cad9eebfd103497958e9d6
Pierre <77687289+xen-cyber@users.noreply.github.com>
2023-04-04 19:30:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,16 @@ const { Client } = require('discord.js-selfbot-v13');
 const { Constants: { APIErrors }} = require('discord.js');
 require('dotenv').config()
 const Cooldown = new Set();
+let mytimer;
+let timer = false;
 
 const client = new Client({
   disableEveryone: true
 });
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
@@ -13,28 +19,27 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; 
 }
 
-mytimer = setInterval(async () => {
+async function bump() {
   var channel = client.channels.cache.get(process.env.CHANNELID);
-  console.log('Bumped Here! #-#-#')
-  channel.sendSlash('302050872383242240', 'bump')
-}, getRandomIntInclusive(7200000, 9600000))
-// 7200000, 9600000
+  console.log('Bumped Here! #-#-#');
+  channel.sendTyping();
+  await sleep(getRandomIntInclusive(2300, 8000));
+  await channel.sendSlash(process.env.DISBOARD_BOT_ID, 'bump')
+}
 
-client.on('ready',  async() => {
-    console.log('First bump takes 2 to 3 hours to come in.')
-    client.user.setStatus('invisible')
-})
+function startBumpTimer() {
+  client.user.setStatus('invisible');
+  mytimer = setInterval(bump, getRandomIntInclusive(7500000, 9600000));
+}
+
+client.on('ready', startBumpTimer);
 
 client.on('message', message => {
-  if (message.channel.id === 'process.env.CHANNELID' && message.type === 'APPLICATION_COMMAND' && message.author.id === '302050872383242240' && message.interaction.commandName === 'bump' && message.interaction.user.id != 'process.env.YourUserID') {
-    console.log('Detected Bump! Resetting...')
-    clearInterval(mytimer)
-    mytimer = setInterval(async () => {
-      var channel = client.channels.cache.get(process.env.CHANNELID);
-      console.log('Bumped Here! #+#+#')
-      channel.sendSlash('302050872383242240', 'bump')
-    }, getRandomIntInclusive(7200000, 9600000))
+    if (message.channel.id === process.env.CHANNELID && message.type === 'APPLICATION_COMMAND' && message.author.id === process.env.DISBOARD_BOT_ID && message.interaction.commandName === 'bump' && message.interaction.user.id != process.env.YourUserID) {
+    console.log('Detected Bump! Resetting...');
+    clearInterval(mytimer);
+    startBumpTimer();
   }
-})
+});
 
-client.login('process.env.TOKEN');
+client.login(process.env.TOKEN);
